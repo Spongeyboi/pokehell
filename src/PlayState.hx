b1b607227ccfdbedfa902c3a1d9c679c85ecab0b
Spongey <71215126+Spongeyboi@users.noreply.github.com>
2022-03-02 08:53:12 -0500

    Fix

--- src/PlayState.hx
+++ src/PlayState.hx
@@ -10,11 +10,17 @@ import flixel.math.FlxPoint;
 class PlayState extends FlxState
 {
     var eevee:Player;
+
+    override function create()
+    {
+        super.create();
+        eevee = new Player(20, 20);
+        add(eevee);
+    }
     
     override function update(elapsed:Float)
     {
         super.update(elapsed);
-        eevee = new Player(20, 20);
-        add(eevee);
+        
     }
 }
